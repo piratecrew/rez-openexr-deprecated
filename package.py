8a3ef68f6d8d13b4adf0c5ebb54d7e1b6c03a299
Fredrik Brännbacka <fredrik@madcrew.se>
2016-02-17 15:00:34 +0100

    Added missing cmake files.

--- package.py
+++ package.py
@@ -8,7 +8,11 @@ description = \
     """
 
 variants = [
-    ["platform-linux"]
+    ["platform"]
+]
+
+build_requires = [
+    "ilmbase-2.1.0"
 ]
 
 requires = [
@@ -18,5 +22,6 @@ requires = [
 uuid = "repository.openexr"
 
 def commands():
+    env.CMAKE_MODULE_PATH.append("{root}/cmake")
     env.PATH.prepend("{root}/bin")
     env.LD_LIBRARY_PATH.prepend("{root}/lib")
